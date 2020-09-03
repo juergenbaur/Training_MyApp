c6999caa8a7c5056956a662f224946eadce55150
Baur Juergen <juergen.baur@avisco.de>
2020-09-03 09:16:22 +0000

    Week1_U3

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,18 +3,12 @@ sap.ui.define([
 	"sap/m/MessageToast"
 ], function (Controller, MessageToast) {
 	"use strict";
-
 	return Controller.extend("opensap.myapp.controller.App", {
 
-		onShowHello : function () {
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-			// show message
-			MessageToast.show(sMsg);
-
+		onShowHello: function () {
+			// show a native JavaScript alert
+			MessageToast.show("Hello openSAP");
 		}
 	});
+
 });
